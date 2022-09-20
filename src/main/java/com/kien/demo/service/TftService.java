563dac1c5dc6de8a5cb7e29fecb9e7c09f1c73a1
Kamii0909 <htk0901htk@gmail.com>
2022-09-20 23:37:53 +0700

    - service layer

--- src/main/java/com/kien/demo/service/TftService.java
+++ src/main/java/com/kien/demo/service/TftService.java
@@ -1,116 +1,27 @@
 package com.kien.demo.service;
 
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Optional;
-import java.util.function.Predicate;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Service;
+import com.kien.demo.model.*;
 
-import com.kien.demo.dao.LocalDatabase;
-import com.kien.demo.dao.TftDao;
-import com.kien.demo.model.Trait;
-import com.kien.demo.model.Unit;
+public interface TftService {
 
+    public List<Trait> allTraits();
+    public List<Unit> allUnits();
 
-@Service
-public class TftService {
+    public List<Trait> traitOf(Unit unit);
+    public List<Unit> unitOf(Trait trait);
 
-    private final TftDao tftDao;
+    public List<Unit> adjaUnit(Unit unit);
 
-    @Autowired
-    public TftService(@Qualifier("Local") TftDao tftDao) {
-        this.tftDao = tftDao;
-    }
+    public LinkedList<Unit> getPathAsList(Unit src, Unit des);
+    public int getPathLength(Unit src, Unit des);
+    public LinkedHashMap<String, Double> avgPathLength(Collection<Unit> comp);
 
-    public HashMap<String, Double> getAverageConnectedScore(List<Unit> units) {
-        int[][] distance = tftDao.getDistance();
-        int total = units.size() - 1;
-        HashMap<String, Double> avgCS = new HashMap<>();
-
-        for(Unit u: units){
-            avgCS.compute(u.getName(), (k, v)-> {
-                v = Double.valueOf(0);
-                for(Unit other: units) 
-                    v += distance[u.getPosition()][other.getPosition()];
-                return v/total;
-            });
-        }
-
-        avgCS.put("Total", avgCS.values().stream().mapToDouble(s-> s).average().orElse(0));
-        
-        return avgCS;
-    }
-
-    public HashMap<String, Double> getAverageConnectedScore() {
-        HashMap<String, Double> avgCS = new HashMap<>();
-        for(Unit u: LocalDatabase.allUnits.values())
-            avgCS.put(u.getName(), Arrays.stream(LocalDatabase.distance[u.getPosition()]).average().orElse(-1)) ;
-        return avgCS;
-    }
-
-    public Collection<Trait> getAllTraits(){
-        return tftDao.getAllTraits();
-    }
-
-    public Collection<Trait> getTraitsWith(Predicate<? super Trait> pred){
-        return tftDao.getTraitsWith(pred);
-    }
-
-    public Collection<Trait> getTraitsContain(String name){
-        return getTraitsWith(t -> t.getName().contains(name));
-    }
-
-    public Optional<Trait> getTraitsExact(String name){
-        return getTraitsWith(t -> t.getName().equals(name)).stream().findFirst();
-    }
-
-    public Collection<Trait> getTraitsContain(Unit unit){
-        return getTraitsWith(t -> t.getUnitsWithThisTrait().contains(unit));
-    }
-
-    public Collection<Trait> getTraitsContain(Unit... units){
-        return getTraitsWith(t -> t.getUnitsWithThisTrait().containsAll(Arrays.asList(units)));
-    }
-
-    public String ignoreATrait(Trait trait){
-        return tftDao.ignoreATrait(getTraitsExact(trait.getName()).orElse(null)) == 1 ?
-         "No trait matching argument" : "Success";
-    }
-
-    public Collection<Unit> getAllUnits(){
-        return tftDao.getAllUnits();
-    }
-
-    public Collection<Unit> getUnitsWith(Predicate <? super Unit> pred){
-        return tftDao.getUnitsWith(pred);
-    }
-
-    public Collection<Unit> getUnitsContain(String name){
-        return getUnitsWith(u -> u.getName().contains(name));
-    }
-
-    public Optional<Unit> getUnitsExact(String name){
-        return getUnitsWith(u -> u.getName().equals(name)).stream().findFirst();
-    }
-    
-    public Collection<Unit> getUnitsContain(Trait trait){
-        return getUnitsWith(u -> u.getTraits().contains(trait));
-    }
-
-    public Collection<Unit> getUnitsContain(Trait...traits){
-        return getUnitsWith(u -> u.getTraits().containsAll(Arrays.asList(traits)));
-    }
-
-    public Collection<Unit> getUnitsConnectedWith(Unit unit){
-        return getUnitsWith(u -> u.getConnectedUnit().contains(unit));
-    }
-
-    public Collection<Unit> getUnitsConnectedWith(Unit...units){
-        return getUnitsWith(u -> u.getConnectedUnit().containsAll(Arrays.asList(units)));
-    }
+    public void removeATrait(Trait trait);
+    public void removeATrait(Unit unit, Trait trait);
+    public void addATrait(Unit unit, Trait trait);
 }
